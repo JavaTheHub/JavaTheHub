a782f46a4bf63c9b94098033fc47b74e03d54fde
Brittany Hemming <brittany.hemming@gmail.com>
2015-05-29 14:56:18 -0400

    editing styling on homepage, footer

--- wp-content/themes/java_the_hub_template/home.php
+++ wp-content/themes/java_the_hub_template/home.php
@@ -2,22 +2,31 @@
 <?php get_header(); ?>
 
 <div class="main">
+<div class="about">
+    <?php // Start the loop ?>
+      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+
+        <h2><?php the_title(); ?></h2>
+        <?php the_content(); ?>
+
+      <?php endwhile; // end the loop?>
+    </div> <!-- end of about -->
+</div><!--end of main-->
+<div class="hero2">
+    
+</div>
   <div class="container">
-
-    <div class="content" data-midnight="black">
-
-    	<h2>The History of Coffee</h2>
+        <h2>The History of Coffee</h2>
     	<p>Legend has it that coffee was first discovered around the 9th century in the Ethiopian highlands. A shepherd named Kaldi noticed energizing effects when his flock nibbled at the red berries of a certain bush. Upon ingesting the fruit himself, he noticed the same energizing effects. Kaldi then took the beans to a local monastery where the monk disapproved and threw them into the fire. The beans gave off such a lovely aroma that it attracted the other monks. They scooped the beans out of the fire, ground them up and boiled them in water and the first cup of coffee was born.</p>
     	<p>Coffee beans were first exported from Ethiopia to Yemen, where Yemeni traders brought coffee back to their homeland and began cultivating it. Sufi’s in Yemen used the beverage for religious purposes as an aid to concentration and also to keep themselves alert during nighttime devotions. Coffee then spread from present day Yemen, northward to Mecca and Medina and then to the larger cities of Cairo, Damascus, Baghdad, and Constantinople. During the 16th century it had already reached the rest of the Middle East. From the Middle East coffee drinking spread to Italy, and then to the rest of Europe. It was then transported by the Dutch to the East Indies and to the Americas.</p>
     	<h3>Arabica VS Robusta Coffee</h3>
     	<p>Arabica coffee or “mountain coffee” makes up 75-80% of the worlds coffee production. It is superior in taste but has less caffeine than Robusta. It grows at an altitude of 600m to 2200m needs between 1200mm to 2200mm of rainfall and needs a temperature of between 15 and 22 degrees Celsius to grow.</p>
     	<p>Robusta coffee is lower quality in taste but has a higher caffeine concentration. You will find that most brands of instant coffee are made entirely of Robusta. It makes up about 20-25% of the worlds coffee production and typically grows in lower altitudes between 0 to 800m, needs between 2200mm to 3000mm of rainfall and grows in warmer climates where the temperature is between 18-36 degrees Celsius.</p>
 
-    </div> <!--/.content -->
+      </div> <!-- /.container -->
 
-    <?php get_sidebar(); ?>
-
-  </div> <!-- /.container -->
-</div> <!-- /.main -->
+<div class="hero3">
+        
+</div>
 
 <?php get_footer(); ?>
